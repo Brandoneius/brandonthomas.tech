c18dc2b2a968e3b05f1f6b26d64e82972ca03989
Brandoneius <brandmthomas@yahoo.com>
2023-02-14 08:59:06 -0800

    Yoga Site For Portfolio and Molly design

---
+++ yoga_flow.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>My Website</title>
+
+<style>
+  /* change the colors */
+$primary: black;
+$danger: #dd0000;
+$success: lime;
+$warning: orange;
+$info: aqua;
+
+$body-bg: #f3f3f0;
+
+@import "bootstrap";
+
+
+
+body {
+    padding-top: 90px;
+}
+
+section {
+    min-height: 90vh;
+}
+</style>
+
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="main.css">
+
+  </head>
+  <body>
+    <div class="container bg-gradient-primary" >
+      <div class="jumbotron text-center">
+        <img src="flower_only.png" alt="Cool flower" style="z-index: 1; width: 80px;">
+        <div class="container">
+        <h1 class="display-4" style="font-family: 'EB Garamond', serif;">Yoga & Wellness</h1>
+        </div>
+
+        <a href="yoga_flow.php" class="btn btn-primary">Log In</a>
+
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan vestibulum arcu, ut commodo dolor.</p>
+        <hr class="my-4">
+        <p>Learn more about us and what we do.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+      </div>
+
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Yoga & Wellness</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav nav-fill w-100">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan vestibulum arcu, ut commodo dolor.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan vestibulum arcu, ut commodo dolor.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan vestibulum arcu, ut commodo dolor.</p>
+        </div>
+      </div>
+
+      <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+          <span class="text-muted">Copyright &copy; 2023</span>
+        </div>
+      </footer>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
